03442a988e5d47380a1466cecb6d7a61a293428e
Victoria <vica-genrietka@yandex.ru>
2022-08-13 03:24:25 +0300

    Zadacha15

---
+++ ZADACHI/Zadacha15/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите цифру от 1 до 7: ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+ if (dayWeek < 6) Console.WriteLine ("Будний день, иди работай");
+else Console.WriteLine ("Выходной, спи");
