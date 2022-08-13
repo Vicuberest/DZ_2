6ebaa04b2cd2fdf73795e4deb47c272f1f76a728
Victoria <vica-genrietka@yandex.ru>
2022-08-13 12:39:10 +0300

    Zadacha13

---
+++ ZADACHI/Zadacha13/Program.cs
@@ -0,0 +1,12 @@
+﻿string number = Console.ReadLine();
+if (number[0] == '-'){
+    if(number.Length >= 4) Console.WriteLine(number[3]);
+    else Console.WriteLine("Третьей цифры нет");
+}
+else {
+    if(number.Length >= 3) Console.WriteLine(number[2]);
+    else Console.WriteLine("Третьей цифры нет");
+    
+} 
+
+
